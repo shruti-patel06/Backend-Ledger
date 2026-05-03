b3ed51c8a62b74267c59a742ffb45df3fcacb458
Shruti <shruti.8717@gmail.com>
2026-05-04 00:00:28 +0530

    dummy API Created

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ const authRouter = require("./routes/auth.routes");
 const accountRouter = require("./routes/account.routes");
 const transactionRoutes = require("./routes/transaction.routes");
 
+app.get("/",(req,res)=>{
+        res.send(" Ledger Service is Up and running ");
+})
 //Use Routes
 app.use("/api/auth",authRouter);
 app.use("/api/accounts",accountRouter)
